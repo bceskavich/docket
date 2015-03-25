414695cd3b7f4aea506375290aff6d3d1a6a01e6
Prasanna Sunil Kulkarni <prasannakulkarni@live.com>
2015-03-24 22:25:06 -0400

    list view on main page in working

---
+++ app/src/main/java/com/bcpk/docket/ListView/listViewActivity.java
@@ -0,0 +1,121 @@
+package com.bcpk.docket.ListView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.bcpk.docket.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ledzee on 3/24/15.
+ */
+
+
+public class listViewActivity extends Activity implements
+        AdapterView.OnItemClickListener {
+
+   /* public static final String[] titles = new String[] {
+            "Strawberry",
+            "Banana",
+            "Orange",
+            "Mixed",
+            "Banana",
+            "Orange",
+            "Mixed",
+            "Strawberry",
+            "Banana",
+            "Orange",
+            "Mixed" };
+
+    public static final String[] descriptions = new String[] {
+            "It is an aggregate accessory fruit",
+            "It is the largest herbaceous flowering plant",
+            "Citrus Fruit",
+            "Mixed Fruits",
+            "It is an aggregate accessory fruit",
+            "It is the largest herbaceous flowering plant",
+            "Citrus Fruit",
+            "Mixed Fruits",
+            "It is an aggregate accessory fruit",
+            "It is the largest herbaceous flowering plant",
+            "Citrus Fruit",
+            };*/
+
+    public static final String[] titles = new String[] {
+            "Strawberry",
+            "Banana",
+            "Orange",
+            "Mixed",
+            "Yo",
+            "Strawberry",
+            "Banana",
+            "Orange",
+            "Mixed",
+            "Yo"};
+
+    public static final String[] descriptions = new String[] {
+            "It is an aggregate accessory fruit",
+            "It is the largest herbaceous flowering plant",
+            "Citrus Fruit",
+            "Mixed Fruits",
+            "this should work",
+            "It is an aggregate accessory fruit",
+            "It is the largest herbaceous flowering plant",
+            "Citrus Fruit",
+            "Mixed Fruits",
+            "this should work" };
+
+    public static final Integer[] images = {
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher
+
+    };
+
+    ListView listView;
+    List<Location> rowItems;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rowItems = new ArrayList<Location>();
+        for (int i = 0; i < titles.length; i++) {
+            Location item = new Location(images[i], titles[i], descriptions[i]);
+            rowItems.add(item);
+        }
+
+        //listView = (ListView) findViewById(R.layout.activity_main.list);
+        listView = (ListView) findViewById(R.id.list);
+        locationAdapter adapter = new locationAdapter(this,
+                R.layout.singlelocation_item, rowItems);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+                            long id) {
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Item " + (position + 1) + ": " + rowItems.get(position),
+                Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
+    }
+}
---
+++ app/src/main/java/com/bcpk/docket/ListView/locationAdapter.java
@@ -0,0 +1,82 @@
+package com.bcpk.docket.ListView;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bcpk.docket.R;
+
+import java.util.List;
+
+//read about viewholders from tt
+
+/**
+ * Created by ledzee on 3/24/15.
+ */
+/*public class locationAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}*/
+
+public class locationAdapter extends ArrayAdapter<Location> {
+
+    Context context;
+
+    public locationAdapter(Context context, int resourceId,
+                                 List<Location> items) {
+        super(context, resourceId, items);
+        this.context = context;
+    }
+
+    /*private view holder class*/
+    private class ViewHolder {
+        ImageView imageView;
+        TextView txtTitle;
+        TextView txtDesc;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        Location rowItem = getItem(position);
+
+        LayoutInflater mInflater = (LayoutInflater) context
+                .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.singlelocation_item, null);
+            holder = new ViewHolder();
+            holder.txtDesc = (TextView) convertView.findViewById(R.id.desc);
+            holder.txtTitle = (TextView) convertView.findViewById(R.id.title);
+            holder.imageView = (ImageView) convertView.findViewById(R.id.icon);
+            convertView.setTag(holder);
+        } else
+            holder = (ViewHolder) convertView.getTag();
+
+        holder.txtDesc.setText(rowItem.getDesc());
+        holder.txtTitle.setText(rowItem.getTitle());
+        holder.imageView.setImageResource(rowItem.getImageId());
+
+        return convertView;
+    }
+}
