1b0353da1e091373be46560c20dd876089aa8213
Prasanna Sunil Kulkarni <prasannakulkarni@live.com>
2015-03-31 16:46:40 -0400

    Merged in contact activity.

--- app/src/main/java/com/bcpk/docket/MainActivity/ContactView/ContactUs.java
+++ app/src/main/java/com/bcpk/docket/MainActivity/ContactView/ContactUs.java
@@ -1,14 +1,18 @@
 package com.bcpk.docket.MainActivity.ContactView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bcpk.docket.R;
 
@@ -21,17 +25,60 @@ public class ContactUs extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
 
-        TextView personName;
-        TextView message;
+        final EditText personName;
+        final EditText message;
         Button sendEmailButton;
 
-        personName = (TextView) findViewById(R.id.editText);
-        message = (TextView) findViewById(R.id.editText2);
+        personName = (EditText) findViewById(R.id.editText);
+        message = (EditText) findViewById(R.id.editText2);
         sendEmailButton = (Button) findViewById(R.id.sendEmailButton);
 
+        sendEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+                String[] recipients = new String[]{"pkulka02@syr.edu", "",};
+                String personalMessage = message.getText().toString();
+                String name = personName.getText().toString();
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, recipients);
+                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Feedback/Suggestion for Docket!");
+                emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, personalMessage);
+                emailIntent.setType("message/rfc822");
+                emailIntent.setType("text/plain");
+                startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+                finish();
+                //sendEmail();
+            }
+        });
 
 
     }
+
+    /*private void sendEmail() {
+        Log.i("Send email", "");
+
+        String[] TO = {"prasanna.k158@gmail.com"};
+        //String[] CC = {"bceskavich@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        //emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback/Suggestion for Docket!");
+        emailIntent.putExtra(Intent.EXTRA_TEXT,);
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email.", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(ContactUs.this,
+                    "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }*/
 
 
     @Override
