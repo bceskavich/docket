077e4e39c7fb0b957cf0d56b4cad0f51664f3941
Billy Ceskavich <bceskavich@gmail.com>
2015-04-03 10:36:49 -0400

    Working tabs with active/inactive colors on MainActivity

--- app/src/main/java/com/bcpk/docket/SlidingTabLayout.java
+++ app/src/main/java/com/bcpk/docket/SlidingTabLayout.java
@@ -223,6 +223,8 @@ public class SlidingTabLayout extends HorizontalScrollView {
             if (i == mViewPager.getCurrentItem()) {
                 tabView.setSelected(true);
             }
+
+            tabTitleView.setTextColor(getResources().getColorStateList(R.color.tab_selector));
         }
     }
 
